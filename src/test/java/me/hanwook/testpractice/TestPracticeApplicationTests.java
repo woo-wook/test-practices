20e4fd1af243a56b35de35eb8205cae75ff95ee2
Hanwook Ryu <ryu@hanwook.me>
2022-10-01 19:25:19 +0900

    first commit

---
+++ src/test/java/me/hanwook/testpractice/TestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hanwook.testpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
