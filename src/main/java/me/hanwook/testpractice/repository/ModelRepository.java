0df6a4c1b9e2e62dc75e57279394b746995a96e1
Hanwook Ryu <ryu@hanwook.me>
2022-10-03 14:35:04 +0900

    Feat: 모델 제조사 검색 추가

---
+++ src/main/java/me/hanwook/testpractice/repository/ModelRepository.java
@@ -0,0 +1,11 @@
+package me.hanwook.testpractice.repository;
+
+import me.hanwook.testpractice.entity.Manufacturer;
+import me.hanwook.testpractice.entity.Model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ModelRepository extends JpaRepository<Model, Long> {
+    List<Model> findByManufacturer(Manufacturer manufacturer);
+}
---
+++ src/test/java/me/hanwook/testpractice/repository/ModelRepositoryTest.java
@@ -0,0 +1,67 @@
+package me.hanwook.testpractice.repository;
+
+import me.hanwook.testpractice.entity.Manufacturer;
+import me.hanwook.testpractice.entity.Model;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class ModelRepositoryTest {
+
+    @Autowired
+    private ModelRepository modelRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    @Transactional
+    public void 제조사_검색() throws Exception {
+        // given
+        Manufacturer hyundai = Manufacturer.builder()
+                .name("현대")
+                .build();
+
+        entityManager.persist(hyundai);
+
+        Manufacturer kia = Manufacturer.builder()
+                .name("기아")
+                .build();
+
+        entityManager.persist(kia);
+
+        modelRepository.save(
+                Model.builder()
+                        .manufacturer(hyundai)
+                        .name("SONATA")
+                        .build()
+        );
+
+        modelRepository.save(
+                Model.builder()
+                        .manufacturer(hyundai)
+                        .name("AVANTE")
+                        .build()
+        );
+
+        modelRepository.save(
+                Model.builder()
+                        .manufacturer(kia)
+                        .name("K5")
+                        .build()
+        );
+
+        // when
+        List<Model> hyundaiModels = modelRepository.findByManufacturer(hyundai);
+
+        // then
+        assertThat(hyundaiModels.size()).isEqualTo(2);
+    }
+}
