f018f58f55bbbcd87ac35a7e23ee8e7e8299b777
Hanwook Ryu <ryu@hanwook.me>
2022-10-17 19:39:38 +0900

    Feat: 차량 생성 모델없음 예외 테스트 추가

--- src/test/java/me/hanwook/testpractice/service/CarServiceTest.java
+++ src/test/java/me/hanwook/testpractice/service/CarServiceTest.java
@@ -4,23 +4,21 @@ import me.hanwook.testpractice.entity.Car;
 import me.hanwook.testpractice.entity.CarColor;
 import me.hanwook.testpractice.entity.Manufacturer;
 import me.hanwook.testpractice.entity.Model;
+import me.hanwook.testpractice.exception.ModelNotFoundException;
 import me.hanwook.testpractice.repository.CarRepository;
 import me.hanwook.testpractice.repository.ModelRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -63,6 +61,17 @@ class CarServiceTest {
         // then
         assertThat(result.getColor()).isEqualTo(color);
         assertThat(result.getModel()).isEqualTo(model);
+    }
+
+    @Test
+    void 차량_생성_모델_없음_예외() {
+        // given
+        final Long modelId = 1L;
+        final CarColor color = CarColor.RED;
+
+        // when & then
+        assertThatThrownBy(() -> carService.create(modelId, color))
+                .isInstanceOf(ModelNotFoundException.class);
     }
 
 }
