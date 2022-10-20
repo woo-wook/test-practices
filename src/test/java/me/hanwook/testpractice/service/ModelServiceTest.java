f6dbb4a187bf2092af6f2c2b1727baecea4bad23
Hanwook Ryu <ryu@hanwook.me>
2022-10-20 16:00:34 +0900

    Feat: 모델 제조사 없음 테스트 추가

--- src/test/java/me/hanwook/testpractice/service/ModelServiceTest.java
+++ src/test/java/me/hanwook/testpractice/service/ModelServiceTest.java
@@ -1,16 +1,20 @@
 package me.hanwook.testpractice.service;
 
 import me.hanwook.testpractice.entity.Manufacturer;
+import me.hanwook.testpractice.exception.ManufacturerNotFoundException;
 import me.hanwook.testpractice.exception.UnusablePriceException;
 import me.hanwook.testpractice.repository.ManufacturerRepository;
+import me.hanwook.testpractice.repository.ModelRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
@@ -20,6 +24,18 @@ class ModelServiceTest {
     ManufacturerRepository manufacturerRepository;
     @InjectMocks
     ModelService modelService;
+
+    @Test
+    void 모델_생성_제조사_없음() {
+        // given
+        Long manufacturerId = 1L;
+        String name = "SONATA";
+        int price = 100000000;
+
+        // when & then
+        assertThatThrownBy(() -> modelService.create(manufacturerId, name, price))
+                .isInstanceOf(ManufacturerNotFoundException.class);
+    }
 
     @Test
     void 모델_생성_금액_경계값() {
