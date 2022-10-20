d3ebd57a259caa4adcdc4e9a4b55f6176f855218
Hanwook Ryu <ryu@hanwook.me>
2022-10-20 15:42:29 +0900

    Feat: 모델 생성 시 예외 추가

---
+++ src/main/java/me/hanwook/testpractice/exception/ManufacturerNotFoundException.java
@@ -0,0 +1,4 @@
+package me.hanwook.testpractice.exception;
+
+public class ManufacturerNotFoundException extends RuntimeException {
+}
---
+++ src/main/java/me/hanwook/testpractice/exception/UnusablePriceException.java
@@ -0,0 +1,4 @@
+package me.hanwook.testpractice.exception;
+
+public class UnusablePriceException extends RuntimeException {
+}
