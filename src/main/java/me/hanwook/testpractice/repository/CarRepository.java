155029dc91926a93fa28a390614356ded25f47e4
Hanwook Ryu <ryu@hanwook.me>
2022-10-25 18:35:53 +0900

    Comment: 주석 추가

--- src/main/java/me/hanwook/testpractice/repository/CarRepository.java
+++ src/main/java/me/hanwook/testpractice/repository/CarRepository.java
@@ -9,7 +9,18 @@ import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
 
+    /**
+     * 모델과 컬러로 카운트 조회
+     * @param model
+     * @param color
+     * @return
+     */
     long countByModelAndColor(Model model, CarColor color);
 
+    /**
+     * 모델별 차량 전체 조회
+     * @param model
+     * @return
+     */
     List<Car> findByModel(Model model);
 }
